819dfda5a49c1e3e768ba26c7ef89dc3713a8079
Asadullah Khan <53692166+asadsk8r02@users.noreply.github.com>
2023-07-13 18:10:42 -0500

    Time: 51 ms (46.59%), Space: 16.4 MB (41.75%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,21 +1,34 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        l = [[1]]
-        if numRows == 1:
-            return [[1]]
+        # l = [[1]]
+        # if numRows == 1:
+        #     return [[1]]
         
-        for i in range(1, numRows):
-            first = l[i - 1][0]
-            last = l[i - 1][-1]
+        # for i in range(1, numRows):
+        #     first = l[i - 1][0]
+        #     last = l[i - 1][-1]
 
-            l_temp = []
-            for k in range(len(l[i - 1]) - 1):
-                l_temp.append(l[i - 1][k] + l[i - 1][k + 1])
+        #     l_temp = []
+        #     for k in range(len(l[i - 1]) - 1):
+        #         l_temp.append(l[i - 1][k] + l[i - 1][k + 1])
 
-            l_temp.insert(0, first)
-            l_temp.append(last)
+        #     l_temp.insert(0, first)
+        #     l_temp.append(last)
 
-            l.append(l_temp)
+        #     l.append(l_temp)
         
-        return l
+        # return l
+        triangle = []
+    
+        for row in range(numRows):
+            # Create a new row with 1s at the beginning and end
+            new_row = [1] * (row + 1)
+            
+            # Fill the middle elements of the row
+            for i in range(1, row):
+                new_row[i] = triangle[row - 1][i - 1] + triangle[row - 1][i]
+            
+            triangle.append(new_row)
+        
+        return triangle
 
