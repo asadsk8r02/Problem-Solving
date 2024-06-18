0b29233e6190ede7c12e4ddf35de961ef360de40
Asadullah Khan <53692166+asadsk8r02@users.noreply.github.com>
2024-06-18 06:24:49 -0500

    Update common-in-3-sorted-arrays.py

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.py
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.py
@@ -2,21 +2,44 @@
 
 class Solution:
     def commonElements (self,A, B, C, n1, n2, n3):
-        new_list = list(set(A)) + list(set(B)) + list(set(C))
-        count_map = {}
-        result = []
-        n = len(new_list)
-        for i in range(n):
-            if new_list[i] not in count_map:
-                count_map[new_list[i]] = 1
-            else:
-                count_map[new_list[i]] += 1
+        # new_list = list(set(A)) + list(set(B)) + list(set(C))
+        # count_map = {}
+        # result = []
+        # n = len(new_list)
+        # for i in range(n):
+        #     if new_list[i] not in count_map:
+        #         count_map[new_list[i]] = 1
+        #     else:
+        #         count_map[new_list[i]] += 1
                 
-            if count_map[new_list[i]]==3:
-                result.append(new_list[i])
+        #     if count_map[new_list[i]]==3:
+        #         result.append(new_list[i])
                 
 
-        return sorted(result)
+        # return sorted(result)
+        i = 0
+        j = 0
+        k = 0
+        result = []
+        
+        while i < n1 and j < n2 and k < n3:
+            if A[i] == B[j] == C[k]:
+                if len(result) == 0 or result[-1] != A[i]:
+                    result.append(A[i])
+                i+=1
+                j+=1
+                k+=1
+            elif A[i] < B[j] or A[i] < C[k]:
+                i += 1
+            elif B[j] < A[i] or B[j] < C[k]:
+                j += 1
+            else:
+                k += 1
+                
+        if len(result) == 0:
+            return [-1]
+    
+        return result 
                 
         
         # your code here
