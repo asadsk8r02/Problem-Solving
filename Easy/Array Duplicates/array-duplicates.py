3982c15ae1ab8d1c24e894ea91f1cdb4bb90e693
Asadullah Khan <53692166+asadsk8r02@users.noreply.github.com>
2024-06-17 23:59:31 -0500

    Update array-duplicates.py

--- Easy/Array Duplicates/array-duplicates.py
+++ Easy/Array Duplicates/array-duplicates.py
@@ -1,21 +1,38 @@
 class Solution:
     def duplicates(self, arr, n): 
-        result = []
-        for i in range(n):
-            index = arr[i] % n
-            arr[index] += n
+        # result = []
+        # for i in range(n):
+        #     index = arr[i] % n
+        #     arr[index] += n
 
-        # Step 2: Collect all indices that have a value greater than or equal to 2*n
-        result = []
-        for i in range(n):
-            if arr[i] // n > 1:
-                result.append(i)
+        # # Step 2: Collect all indices that have a value greater than or equal to 2*n
+        # result = []
+        # for i in range(n):
+        #     if arr[i] // n > 1:
+        #         result.append(i)
 
-        # Step 3: If no duplicates, return [-1]
-        if not result:
-            return [-1]
+        # # Step 3: If no duplicates, return [-1]
+        # if not result:
+        #     return [-1]
         
-        return result
+        # return result
+
+        count_map = {}
+        for num in arr:
+            if num in count_map:
+                count_map[num] += 1
+            else:
+                count_map[num] = 1
+        
+        result = []        
+        for key, value in count_map.items():
+            if value > 1:
+                result.append(key)
+        
+        if result:
+            return sorted(result)
+        else:
+            return [-1]
                 
     	# code here
     	    
